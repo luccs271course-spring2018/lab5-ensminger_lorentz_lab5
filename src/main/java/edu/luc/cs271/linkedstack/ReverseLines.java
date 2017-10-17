d3ec26341fd55daec30eec3058de3ede7c932c16
Ben Lorentz <blorentz@luc.edu>
2017-10-16 22:44:17 -0500

    Final commit with Answers.md

--- src/main/java/edu/luc/cs271/linkedstack/ReverseLines.java
+++ src/main/java/edu/luc/cs271/linkedstack/ReverseLines.java
@@ -1,5 +1,6 @@
 package edu.luc.cs271.linkedstack;
 
+import java.util.*;
 import java.util.Scanner;
 
 public class ReverseLines {
@@ -16,6 +17,7 @@ public class ReverseLines {
       stack.push(line);
     }
     String response = "";
+
     while (!stack.isEmpty()) {
       response = response + stack.pop();
       System.out.println(response);
